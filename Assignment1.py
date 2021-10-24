027345c046491f545a20688df06388662fbf3e6a
Keith Butler <20089137@mail.wit.ie>
2021-10-24 21:54:35 +0100

    index.html showing metadata

--- Assignment1.py
+++ Assignment1.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
 
-# TODO: change from os.system (Depreciated)
-
 import sys
-import os
+import subprocess
 import boto3
 import webbrowser
 
 ec2 = boto3.resource('ec2')
 s3 = boto3.resource("s3")
+s3_client = boto3.client("s3")
+# TODO: add cloudwatch
 
-# projectx-bucket1-$(date +'%F'-'%s')
+# assignment01-bucket1-$(date +'%F'-'%s')
 bucket_name = sys.argv[1]
-# object_name = sys.argv[2]
+
 
 def main() -> None:
-    # createInstance()
-    createBucket()
-    putInBucket()
-    # webbrowser.open('http://ipaddress')  # Cross platform
-    # webbrowser.open(url, new=0, autoraise=True)
+    ec2Setup()
+    # s3Setup()
 
-def createInstance() -> None:
+def ec2Setup() -> None:
+    """Creates an ec2 instance using the users aws-cli config"""
     # ec2 setup script
     user_data ='''#!/bin/bash
+    sudo yum update -y;
     sudo yum install httpd -y;
     sudo systemctl enable httpd;
     sudo systemctl start httpd;
@@ -36,58 +35,135 @@ def createInstance() -> None:
             MaxCount=1,
             InstanceType='t2.nano',
             KeyName='kbutler_key',
-            SecurityGroupIds=['sg-002ec529f411873fa'],
+            # SecurityGroupIds=['sg-002ec529f411873fa'],
+            SecurityGroupIds=['assign-sec-group'],
             TagSpecifications=[{
                     'ResourceType': 'instance',
                     'Tags': [{'Key':'Name','Value':'Assignment 01 Server'},]
                 }],
             UserData=user_data,
         )
-        # instance.wait_until_running()
+        print ('Waiting until running')
+        instance[0].wait_until_running()
+        print ('Reloading')
+        instance[0].reload()
+        print ('Waiting until running')
+        instance[0].wait_until_running()
         print ('ec2 instance created.')
-        print ('Instance ID:', instance[0].id)
-        print ('Opening ec2 website in web browser.')
-        webbrowser.open('http://' + instance.public_ip_address) # Cross platform
     except Exception as error:
-        print (error)
+        print(error)
+        print('Instance creation error')
 
-def createBucket() -> None:
-    print ('Creating the s3 bucket.')
+    try:
+        instanceIP = instance[0].public_ip_address
+        print ('Instance ID:', instance[0].id, '\nInstance IP:', instanceIP)
+
+        # TODO
+
+        # Adding required files to the instance
+        print('Curling image')
+        subprocess.run(["ssh -o StrictHostKeyChecking=no ec2-user@" + instanceIP + " 'curl -o image.jpg http://devops.witdemo.net/image.jpg'"],shell=True)
+        print('Copying index.html')
+        subprocess.run(["scp " + "index.html " + "ec2-user@" + instanceIP + ":/home/ec2-user/index.html"],shell=True)
+        # subprocess.run(["ssh " + "ec2-user@" + instanceIP + " 'sudo mv index.html /var/www/html/'"],shell=True)
+        print('Moving index.html')
+        subprocess.run(["ssh -t ec2-user@" + instanceIP + " 'sudo mkdir /var/www'"],shell=True)
+        subprocess.run(["ssh -t ec2-user@" + instanceIP + " 'sudo mkdir /var/www/html'"],shell=True)
+        subprocess.run(["ssh -t ec2-user@" + instanceIP + " 'sudo mv ./* /var/www/html'"],shell=True)
+    except Exception as error:
+        print(error)
+
+    try:
+        # TODO
+        # Metadata
+        # subprocess.run(["ssh ec2-user@" + instanceIP + " 'curl http://" + instanceIP + "/latest/meta-data/local-ipv4 >> index.html'"],shell=True)
+        print ('Adding metadata to index file')
+        subprocess.run(["ssh ec2-user@" + instanceIP + " 'sudo curl http://169.254.169.254/latest/meta-data/local-ipv4 >> /var/www/html/index.html'"],shell=True)
+    except Exception as error:
+        print(error)
+        print('Metadata')
+
+    try:
+        # Monitor
+        # TODO
+        print ('Copying monitor.sh to instance.')
+        subprocess.run(["scp ", "monitor.sh", "ec2-user@" + instanceIP + ":/home/ec2-user/monitor.sh"],shell=True)
+        # setting monitor file permissions
+        print ('Setting correct permissions')
+        subprocess.run(["ssh ec2-user@" + instanceIP + " 'chmod 700 monitor.sh'"],shell=True)
+        print ('Running monitor.sh')
+        subprocess.run(["ssh -o ec2-user@" + instanceIP + " './monitor.sh'"],shell=True)
+    except Exception as error:
+        print(error)
+        print('Monitoring failed')
+
+    try:
+        print ('Opening ec2 website in web browser.')
+        webbrowser.open('http://' + instanceIP) # Cross platform
+    except Exception as error:
+        print(error)
+        print('Failed to open website in web browser')
+
+
+def s3Setup() -> None:
+    print ('setting bucket name')
+    # bucket_name = 'awdqqueqjdwadd38r2jdwwa'
     for bucket_name in sys.argv[1:]:
         try:
-            response = s3.create_bucket(
-                Bucket=bucket_name, CreateBucketConfiguration={
+            print ('Creating s3 bucket')
+            bucket = s3_client.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration={
                     'LocationConstraint': 'eu-west-1'
                 }
             )
-            # Waits for the bucket to exist
-            response = s3.Waiter.BucketExists(bucket=bucket_name)
-            print ('s3 bucket created.')
-            print (response)
-        except Exception as error:
-            print (error)
+            # print (response)
+        except :
+            print ('Cannot create bucket.')
+        try:
+            print ('Setting bucket policy')
+            bucket_policy = {
+                'Version': '2012-10-17',
+                'Statement': [{
+                    'Sid': 'AddPerm',
+                    'Effect': 'Allow',
+                    'Principal': '*',
+                    'Action': ['s3:GetObject'],
+                    'Resource': "arn:aws:s3:::%s/*" % bucket_name
+                }]
+            }
+            bucket_policy = json.dumps(bucket_policy)
+            s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy)
 
-def putInBucket() -> None:
-    try:
-        # Sets the s3 bucket as a static website
-        print ('Enabling website functionality.')
-        response = s3.BucketWebsite(bucket_name)
-        # Downloads the required image
-        print ('Downloading required files.')
-        os.system('wget http://devops.witdemo.net/assign1.jpg')
-        # Adds two files from the current directory  to the s3 bucket
-        print ('Sending files to s3 bucket.')
-        response = s3.Object(bucket_name, 'assign1.jpg').put(Body=open('assign1.jpg', 'rb'))
-        print (response)
-        response = s3.Object(bucket_name, 'index.html').put(Body=open('index.html', 'rb'))
-        print (response)
+            print ('Setting up bucket website')
+            s3_client.put_bucket_website(
+                Bucket=bucket_name,
+                WebsiteConfiguration={
+                'ErrorDocument': {'Key': 'error.html'},
+                'IndexDocument': {'Suffix': 'index.html'},
+                }
+            )
+        except:
+            print ('Cannot setup website.')
+
+        waiter = s3_client.get_waiter('bucket_exists')
+        waiter.wait(Bucket=bucket_name)
+        # bucket.wait_until_exists()
+
+        filename = ['index.html','error.html']
+        print ('Uploading an image to s3 bucket')
+        s3.Object(bucket_name,'assign1.jpg').upload_file(Filename='assign1.jpg',ExtraArgs={'ContentType':'image/jpeg'})
+        for file in filename:
+            try:
+                print ('Uploading a file to s3 bucket')
+                s3.Object(bucket_name,file).upload_file(Filename=file,ExtraArgs={'ContentType':'text/html'})
+                # print(response)
+            except Exception as error:
+                print (error)
+                print('Failed to put file into bucket.')
+
         print ('Opening s3 website in web browser.')
         webbrowser.open('http://' + bucket_name + '.s3-website-eu-west-1.amazonaws.com')  # Cross platform
-        print (response)
-        print ('Removing unneaded file from host computer.')
-        os.system('rm assign1.jpg') # Cleaning up directory
-    except Exception as error:
-        print (error)
 
 
 if __name__ == "__main__":
